701333a0117ade49704c695e69a3f591bcba55c9
Drew Coleman <dcolem12@my.westga.edu>
2018-12-04 13:42:17 -0500

    More testing added - Strength constructor fixed to fully utilize the superclass

--- src/character/abilities/Strength.java
+++ src/character/abilities/Strength.java
@@ -2,4 +2,11 @@ package character.abilities;
 
 public class Strength extends Ability {
 	
+	public Strength() {
+		super();
+	}
+	
+	public Strength(int acceptedStrengthScore) {
+		super(acceptedStrengthScore);
+	}
 }
--- src/character/ability/testing/TestAbility.java
+++ src/character/ability/testing/TestAbility.java
@@ -16,7 +16,57 @@ class TestAbility {
 	 */
 	@Test
 	void testAbilityGetScoreWithDefaultScores() {
-		assertEquals(strength.getScore(), 10);
+		assertEquals(this.strength.getScore(), 10);
+	}
+	
+	/**
+	 * This test will check the constructor by calling the getScore method
+	 * All values called will be positive 
+	 */
+	@Test
+	void testAbilitySecondConstructorWithPositiveValues() {
+		Ability strength2 = new Strength(2);
+		assertEquals(strength2.getScore(), 2);
+		Ability strength20 = new Strength(20);
+		assertEquals(strength20.getScore(), 20);
+		Ability strength200 = new Strength(200);
+		assertEquals(strength200.getScore(), 200);
 	}
 
+	
+	/**
+	 * This test will check the constructor by calling the getScore method
+	 * All values called will be Negative
+	 */
+	@Test
+	void testAbilitySecondConstructorWithNegativeValues() {
+		Ability strength7 = new Strength(-7);
+		assertEquals(strength7.getScore(), -7);
+		Ability strength70 = new Strength(-70);
+		assertEquals(strength70.getScore(), -70);
+		Ability strength700 = new Strength(-700);
+		assertEquals(strength700.getScore(), -700);
+	}
+	
+	/**
+	 * This test will insure that the addScore method successfully adds the accepted value to the ability score
+	 */
+	@Test
+	void testAbilityAddScore() {
+		assertEquals(this.strength.getScore(), 10);
+		this.strength.addScore(5);
+		assertEquals(this.strength.getScore(), 15);
+	}
+	
+	/**
+	 * This test will insure that the addScore method successfully adds the accepted value to the ability score
+	 * Negative values tested
+	 */
+	@Test
+	void testAbilityAddScoreWithNegativeValues() {
+		assertEquals(this.strength.getScore(), 10);
+		this.strength.addScore(-6);
+		assertEquals(this.strength.getScore(), 4);
+		
+	}
 }
